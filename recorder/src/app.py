599cc9b06d661ec7d0b4f579f98ef31482825451
Giorgio Groß <uvech@student.kit.edu>
2017-05-22 16:06:27 +0200

    Add basic UI

--- recorder/src/app.py
+++ recorder/src/app.py
@@ -1,11 +1,23 @@
 from reader import AsyncReader
 from processor import Processor
+import Tkinter
 
 
-def rec(input):
-    dp.put_raw(input)
+def process_input(in_stream):
+    dataProcessor.put_raw(in_stream)
 
-dp = Processor.DataProcessor()
 
-ar = AsyncReader.StdinReader(0, "reader", rec)
+def start_recording():
+    dataProcessor.record(int(gestureIdEntry.get()))
+
+
+dataProcessor = Processor.DataProcessor()
+ar = AsyncReader.StdinReader(0, "reader", process_input)
 ar.start()
+
+top = Tkinter.Tk()
+startButton = Tkinter.Button(top, text="GO!", command=start_recording)
+startButton.pack(side=Tkinter.LEFT)
+gestureIdEntry = Tkinter.Entry(top)
+gestureIdEntry.pack(side=Tkinter.RIGHT)
+top.mainloop()
